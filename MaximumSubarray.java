b6a22ec9d338a3b7f3d7df8232d7a23fbac26b02
LIU <liuyf211@gmail.com>
2017-09-22 02:55:26 -0500

    add divide & conquer method to calculate the maximum subarray

--- MaximumSubarray.java
+++ MaximumSubarray.java
@@ -6,11 +6,68 @@ public class MaximumSubarray {
 
         MaximumSubarray maximumSubarray = new MaximumSubarray();
         TestHelper.printArray(prices);
+
 //        int result = maximumSubarray.calculate1(prices);
-        int result = maximumSubarray.calculate2(prices);
-        System.out.println("\nmaximum profit = [" + result + "]");
+//        int result = maximumSubarray.calculate2(prices);
+
+        int[] arrayDifferences = new int[prices.length];
+        for (int i = 1; i < prices.length; i++) {
+            arrayDifferences[i] = prices[i] - prices[i - 1];
+        }
+
+        System.out.println("\n");
+        TestHelper.printArray(arrayDifferences);
+
+        int[] result = maximumSubarray.findMaximumSubarray(arrayDifferences, 0, arrayDifferences.length - 1);
+        System.out.println("\nresult = [" + result[0] + "," + result[1] + "," + result[2] + "]");
     }
 
+    // Divide & Conquer
+    // the format of return : low index, high index, the sum of maximum subarray
+    // if used in the Stock example, the return value "low" should minus 1, then the index
+    // (low -1) can be the day to buy stock
+    public int[] findMaximumSubarray(int[] arr, int low, int high) {
+        if (low < high) {
+            int mid = (low + high) / 2;
+            int[] left = findMaximumSubarray(arr, low, mid);
+            int[] right = findMaximumSubarray(arr, mid + 1, high);
+            int[] cross = findCrossMaximumSubarray(arr, low, mid, high);
+            if (left[2] >= right[2] && left[2] >= cross[2]) {
+                return left;
+            } else if (right[2] >= left[2] && right[2] >= cross[2]) {
+                return right;
+            } else {
+                return cross;
+            }
+        }
+        return new int[]{low, high, arr[low]};
+    }
+
+    public int[] findCrossMaximumSubarray(int[] arr, int low, int mid, int high) {
+        int leftSum = Integer.MIN_VALUE;
+        int sum = 0;
+        int leftIndex = low;
+        for (int i = mid; i >= low; i--) {
+            sum += arr[i];
+            if (sum > leftSum) {
+                leftSum = sum;
+                leftIndex = i;
+            }
+        }
+        int rightSum = Integer.MIN_VALUE;
+        sum = 0;
+        int rightIndex = high;
+        for (int i = mid + 1; i <= high; i++) {
+            sum += arr[i];
+            if (sum > rightSum) {
+                rightSum = sum;
+                rightIndex = i;
+            }
+        }
+        return new int[]{leftIndex, rightIndex, rightSum + leftSum};
+    }
+
+    // Method 2
     public int calculate1(int[] prices) {
         if (prices.length < 2) return 0;
         int sum = 0, maxProfit = 0;
@@ -21,12 +78,13 @@ public class MaximumSubarray {
         return maxProfit;
     }
 
-    public int calculate2(int[] prices){
+    // Method 3
+    public int calculate2(int[] prices) {
         if (prices.length < 2) return 0;
         int min = prices[0], maxProfit = 0;
-        for(int i =1;i<prices.length;i++){
+        for (int i = 1; i < prices.length; i++) {
             min = Math.min(min, prices[i]);
-            maxProfit = Math.max(maxProfit, prices[i]-min);
+            maxProfit = Math.max(maxProfit, prices[i] - min);
         }
         return maxProfit;
     }
--- README.md
+++ README.md
@@ -1,21 +1,19 @@
 # CLRS-Algorithms
 Some algorithm implementations of the book: "Introduction to Algorithms"
 
-### 1. Insertion Sort
+####1. Insertion Sort
+
+****
+
+####2. Merge Sort - divide & conquer
 
 ***
 
-### 2. Merge Sort - divide & conquer
-
-***
-
-### 3. The Maximum Subarray problem — divide & conquer
+####3. The Maximum Subarray problem — divide & conquer
 
 * LeetCode — Best Time to Buy and Sell Stock
 
   ![Image is from Introduction to Algorithms](./images/maximumSubarrayExample.jpg "Image is from Introduction to Algorithms")
 
-  ​
-
-
+***
 
